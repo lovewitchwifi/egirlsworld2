89c41c22afeeca115aee87556c7ffe3b3a61e69a
lovewitchwifi <mel.blanco.mail@gmail.com>
2023-08-28 23:00:39 +0300

    final update

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ gulp.task('default', (cb) => {
  // SERVE  FRONT END WITH PROXY TO BACKEND
 	browserSync.init({
 	 proxy: {
-		 target: 'http://localhost:8000',
+		 target: 'http://localhost:8800',
 		 ws: true
 	 },
 	 serveStatic: ['./public']
